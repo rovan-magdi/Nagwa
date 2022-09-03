18e5a2312c7f6a49187b940636655e75cb110cf0
rovan_magdi <magdirovan@gmail.com>
2022-09-03 23:43:23 +0200

    edit_frontend

--- frontend/src/pages/practice.js
+++ frontend/src/pages/practice.js
@@ -2,85 +2,99 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import { StyledButton, StyledButtonSubmit } from "../styled/Button";
 import { StyledContainerButtons, StyledWord } from "../styled/Container";
-import { Stack } from "@mui/system";
+import { Stack, Box } from "@mui/system";
 import { useNavigate } from "react-router-dom";
+import { Typography } from "@mui/material";
 
-const Practice= () => {
-    //use store in words that from backend 
-    const [listWords, setlistWords] = useState([]);
+const Practice = () => {
+  //use store in words that from backend
+  const [listWords, setlistWords] = useState([]);
 
-    //use store in questions that answered 
-    const [answeredQuestions, setAnsweredQuestions] = useState([]);
+  //use store in questions that answered
+  const [answeredQuestions, setAnsweredQuestions] = useState([]);
 
-    //collections of buttons
-    const buttons = ["verb", "noun", "adverb", "adjective"];
+  //take name from localstorage
+  const [name, setName] = useState("");
 
-    //use router-dom for navigation
-    const navigate = useNavigate();
+  //collections of buttons
+  const buttons = ["verb", "noun", "adverb", "adjective"];
 
-    // const [isActive, setIsActive] = useState(false);
+  //use router-dom for navigation
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        axios.get(`http://localhost:5000/words`).then((response) => {
-            setlistWords(response.data.words);
-            console.log(response.data.words);
-        });
-    }, []);
+  // const [isActive, setIsActive] = useState(false);
 
-    const selectWord = (e, valueTarget) => {
-        console.log(valueTarget.target.value);
-        if (e.pos === valueTarget.target.value) {
-            //make array unique with set 
-            setAnsweredQuestions((current) => [...new Set(current), e.id]);
-            console.log(answeredQuestions);
-        } else {
-            console.log("false");
-        }
-    };
+  useEffect(() => {
+    axios.get(`http://localhost:5000/words`).then((response) => {
+      setlistWords(response.data.words);
+      console.log(response.data.words);
+    });
+    setName(localStorage.getItem("name"));
+  }, []);
 
-    const calculatedResult = (event) => {
-        //Calutate Practice= number of answered questions / total number of questions)*100
-        let calculated=((answeredQuestions.length / listWords.length) * 100)
-        localStorage.setItem('result',(calculated ));
-        localStorage.setItem('authFlag',true);
-        //alert make sure that exam finish
-        alert("Submitted!");
-        navigate('/result');
+  const selectWord = (e, valueTarget) => {
+    console.log(valueTarget.target.value);
+    if (e.pos === valueTarget.target.value) {
+      //make array unique with set
+      setAnsweredQuestions((current) => [...new Set(current), e.id]);
+      console.log(answeredQuestions);
+    } else {
+      console.log("false");
+    }
+  };
 
-    };
+  const calculatedResult = (event) => {
+    //Calutate Practice= number of answered questions / total number of questions)*100
+    let calculated = (answeredQuestions.length / listWords.length) * 100;
+    localStorage.setItem("result", calculated);
+    localStorage.setItem("authFlag", true);
+    //alert make sure that exam finish
+    alert("Submitted!");
+    navigate("/result");
+  };
 
-    return (
-        <Stack sx={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            flexDirection: "column"
-        }}>
-            {listWords?.map((e, uniqueKey) => {
-                return (
-                   
-                        <StyledContainerButtons key={uniqueKey}>
-                            <StyledWord> {e.word}</StyledWord>
+  return (
+    <Stack
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
+      <Box sx={{ fontSize: "2.3rem", marginTop: "20px" }}>
+        <Typography variant="span" sx={{ fontWeight: "bold" }}>
+          {" "}
+          Welcome -
+        </Typography>
+        <Typography variant="span" sx={{ textTransform: "capitalize" }}>
+          {" "}
+          {name}
+        </Typography>
+      </Box>
 
-                            {buttons?.map((buttonValue, uniqueKey) => {
-                                return (
-                                    <StyledButton
-                                        value={buttonValue}
-                                        key={uniqueKey}
-                                        onClick={(valueTarget) => selectWord(e, valueTarget)}
-                                    >
-                                        {buttonValue}
-                                    </StyledButton>
-                                );
-                            })}
+      {listWords?.map((e, uniqueKey) => {
+        return (
+          <StyledContainerButtons key={uniqueKey}>
+            <StyledWord> {e.word}</StyledWord>
 
-                        </StyledContainerButtons>
-                    
-                );
+            {buttons?.map((buttonValue, uniqueKey) => {
+              return (
+                <StyledButton
+                  value={buttonValue}
+                  key={uniqueKey}
+                  onClick={(valueTarget) => selectWord(e, valueTarget)}
+                >
+                  {buttonValue}
+                </StyledButton>
+              );
             })}
-            <StyledButtonSubmit onClick={calculatedResult}>Submit</StyledButtonSubmit>
-        </Stack>
-    );
+          </StyledContainerButtons>
+        );
+      })}
+      <StyledButtonSubmit onClick={calculatedResult}>Submit</StyledButtonSubmit>
+    </Stack>
+  );
 };
 
 export default Practice;
--- frontend/src/pages/welcome.js
+++ frontend/src/pages/welcome.js
@@ -3,8 +3,14 @@ import { useNavigate } from "react-router-dom";
 import { useAuth } from "../utils/PrivateRoute";
 
 import { TextField, Button, Box, Stack } from "@mui/material";
-let Welcome = (props) => {
+let Welcome = () => {
+  const [username, setUsername] = useState('');
   const navigate = useNavigate();
+  const handleChange = event => {
+    setUsername(event.target.value);
+    localStorage.setItem("name", username);
+
+  };
   let Start = () => {
     navigate("/exam");
   };
@@ -16,11 +22,17 @@ let Welcome = (props) => {
         alignItems: "center",
         justifyItems: "center",
         height: "100vh",
-        
       }}
     >
       <Stack spacing={2}>
-        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+        <TextField
+          id="outlined-basic"
+          label="Outlined"
+          variant="outlined"
+          name="name"
+          value={username}
+          onChange={handleChange}
+        />
         <Button variant="contained" onClick={Start}>
           Start
         </Button>
